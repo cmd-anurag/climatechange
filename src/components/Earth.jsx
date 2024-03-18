471ce53bfb54e80cba2ac03b70de7668883dd0b6
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-03-18 19:16:40 +0530

    added intro animations

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -7,49 +7,57 @@ function Earth() {
   const mountRef = useRef(null);
 
   useEffect(() => {
-    const width = window.innerWidth,
-      height = window.innerHeight;
 
-    // init
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);
-    camera.position.set(2.5, 0, 2);
+    const width = window.innerWidth, height = window.innerHeight;
+
+    // make a camera
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);    
+    camera.position.set(2, 0, -10);
     camera.lookAt(0, 0, 0);
+
+    // make a scene
     const scene = new THREE.Scene();
 
+    // creating a light source
     const light = new THREE.DirectionalLight(0xffffff, 2, 10);
     light.position.set(5, 0, 0);
     scene.add(light);
 
+    // creating another dim light source
     const nightlight = new THREE.AmbientLight(0xffffff, 0.03, 10);
     nightlight.position.set(-5, 0, 0);
     scene.add(nightlight);
 
+    // loading the earth texture
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load(earthtexture);
 
+    // creating and adding the earth
     const SphereGeometry = new THREE.SphereGeometry(1.5, 64, 64);
     const SphereMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
     const sphere = new THREE.Mesh(SphereGeometry, SphereMaterial);
-    scene.add(sphere);
     sphere.position.set(0, 0, 0);
+    scene.add(sphere);
 
-    const gridHelper = new THREE.GridHelper(50, 100);
+    //  Some helper objects
+    // const gridHelper = new THREE.GridHelper(50, 100);
     // scene.add(gridHelper);
-    const plhelper = new THREE.PointLightHelper(light);
-    // scene.add(plhelper);
+    // const plhelper = new THREE.PointLightHelper(light);
+    // scene.add(plhelper); 
 
-    // Create an empty BufferGeometry
+    // creating an empty BufferGeometry
     const starGeometry = new THREE.BufferGeometry();
 
-    // Create an array to store the positions of the stars
+    // storing the positions of the stars
     const starPositions = [];
 
     // Creating 500 stars
     for (let i = 0; i < 1000; i++) {
+
       // Generate a random position for each star
       const x = THREE.MathUtils.randFloatSpread(2000); // spread in a range of -1000 to 1000 on the x-axis
-      const y = THREE.MathUtils.randFloatSpread(2000); // spread in a range of -1000 to 1000 on the y-axis
-      const z = THREE.MathUtils.randFloatSpread(2000); // spread in a range of -1000 to 1000 on the z-axis
+      const y = THREE.MathUtils.randFloatSpread(2000); 
+      const z = THREE.MathUtils.randFloatSpread(2000); 
 
       // Add the position to the array
       starPositions.push(x, y, z);
@@ -58,32 +66,47 @@ function Earth() {
     // Add the positions to the geometry
     starGeometry.setAttribute("position", new THREE.Float32BufferAttribute(starPositions, 3));
 
-    // Create a PointsMaterial
+    // Create the start material
     const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
-
-    // Create a Points object with the geometry and material
     const stars = new THREE.Points(starGeometry, starMaterial);
 
     // Add the stars to the scene
     scene.add(stars);
 
+    // creating the renderer
     const renderer = new THREE.WebGLRenderer();
-
     renderer.setSize(width, height);
     mountRef.current.appendChild(renderer.domElement);
 
+    // creating orbit controls
     const controls = new OrbitControls(camera, renderer.domElement);
 
+    // zoom-in animation
+    const target = new THREE.Vector3(2, 0, 2);
+    let start = null;
+    let isLerping = true;
+
+    // animation loop
     const animate = function (time) {
       requestAnimationFrame(animate);
-      sphere.rotation.y += 0.0035;
-      controls.update();
+    
+      if (isLerping) {
+        camera.position.lerp(target, 0.05);
+        if (camera.position.distanceTo(target) < 0.01) {
+          isLerping = false;
+        }
+        controls.update();
+      }
+    
+      sphere.rotation.y += 0.0025;
       renderer.render(scene, camera);
     };
-
     animate();
+
+    // cleanup function
     return () => {
       mountRef.current.removeChild(renderer.domElement);
+      
     };
   }, []);
 
