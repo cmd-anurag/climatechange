51300ef1c9a543ca6cc70d4bc8becfc364566a31
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-03-21 19:20:39 +0530

    changed bg of /cc

--- src/components/ClimateChange.jsx
+++ src/components/ClimateChange.jsx
@@ -4,11 +4,13 @@ import '../App.css'
 
 const ClimateChange = () => {
   return (
+    <>
     <div className='stackedbg'>
       <div className='climatechange-header container'>
         <h1><span id='climate'>CLIMATE </span><br /><span className='mx-5' id='change'>CHANGE</span></h1>
       </div>
     </div>
+    </>
   )
 }
 
